bf1410cf3da518fc26283167cf8010849a8f9c53
David Fernández <davidfernandezcomesana@gmail.com>
2023-01-19 19:05:17 +0100

    Update home responsive

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,7 @@ import { FaShopify, FaPercentage } from 'react-icons/fa';
 
 const Home = () => {
     return (
-        <section className='mx-10'>
+        <section className='mx-16'>
             <main className='flex ml-10 mt-8'>
                 <div className='flex flex-col w-44 md:w-60'>
                     <h1 className='text-xl md:text-3xl font-bold py-3 mt-16'>
@@ -21,16 +21,16 @@ const Home = () => {
                         </span>
                         Shopping
                     </h1>
-                    <p className='italic mt-4 md:mt-6 text-sm md:text-lg'>Start buying from our suppliers in more than fifteen countries.</p>
+                    <p className='italic mt-4 md:mt-6 text-sm text-center md:text-lg'>Start buying from our suppliers in more than fifteen countries.</p>
                 </div>
                 <img 
                     className='mt-10 w-24 h-24 md:w-auto md:h-auto' 
                     alt='' 
                     src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZTmRvAWLGcLhcHpVeeZDwXk8SlxbZYjAB7w&usqp=CAU' />
             </main>
-            <div className='grid grid-cols-1 md:grid-cols-4 gap-4 my-14 pb-20'>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-4 my-16 pb-20'>
                     <img 
-                        className='w-auto h-auto md:w-100 md:h-60 rounded-3xl shadow-lg shadow-zinc-200 hover:shadow-zinc-400' 
+                        className='w-auto h-auto md:w-80 md:h-50 rounded-3xl shadow-lg shadow-zinc-200 hover:shadow-zinc-400' 
                         alt='' 
                         src='https://static.doofinder.com/main-files/uploads/2020/05/vender-una-tienda-online.jpg' />
                     <div className='text-left px-6 md:px-10 py-5'>
@@ -42,7 +42,7 @@ const Home = () => {
                         <p className='font-extralight italic pt-1 text-sm md:text-lg'>IOS, Android and web.</p>
                     </div>
                     <img 
-                        className='w-auto h-auto md:w-100 md:h-60 rounded-3xl shadow-lg shadow-zinc-200 hover:shadow-zinc-400' 
+                        className='w-auto h-auto md:w-80 md:h-50 rounded-3xl shadow-lg shadow-zinc-200 hover:shadow-zinc-400' 
                         alt='' 
                         src='https://img.freepik.com/vector-gratis/mega-venta-letras-bolsas-compra-carrito_1262-15855.jpg?w=2000' />
                     <div className='text-left px-6 md:px-10 py-5 '>
